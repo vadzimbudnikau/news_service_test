2ecb040bd71253cd0abb6677bdaa67e990290b0f
Vadzim Budnikau <vadimbudnikov@gmail.com>
2024-09-23 22:07:18 +0300

    Created news app and News model

--- news_service/settings.py
+++ news_service/settings.py
@@ -61,6 +61,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+    "rest_framework_simplejwt",
+
+    "news",
 ]
 
 MIDDLEWARE = [
