5a4a9a107bfa03c0d9472eccf575ff45a8e95ed1
Vadzim Budnikau <vadimbudnikov@gmail.com>
2024-09-23 22:53:52 +0300

    Added tests for News API

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from news.models import News
 
-# Create your tests here.
+class NewsAPITests(APITestCase):
+
+    def setUp(self):
+        self.create_url = reverse('news-create')
+        self.news_detail_url = lambda id: reverse('news-detail', args=[id])
+
+    def test_create_news(self):
+        data = {
+            'title': 'Test News',
+            'content': 'This is a test news content.'
+        }
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(News.objects.count(), 1)
+        self.assertEqual(News.objects.get().title, 'Test News')
+
+    def test_get_news(self):
+        news = News.objects.create(title='Test News', content='This is a test news content.')
+        response = self.client.get(self.news_detail_url(news.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], news.title)
+
+    def test_get_nonexistent_news(self):
+        response = self.client.get(self.news_detail_url(999))  # ID that does not exist
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
